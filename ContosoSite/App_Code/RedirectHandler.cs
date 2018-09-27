2d3ce096a39574893cd482f719bf70c1ab0ee668
Ted Wallick <twallick@email.phoenix.edu>
2018-09-27 15:15:16 -0400

    Customizing a View

---
+++ ContosoSite/App_Code/RedirectHandler.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+/// <summary>
+/// Summary description for RedirectHandler
+/// </summary>
+public class RedirectHandler : IHttpHandler
+{
+    public RedirectHandler()
+    {
+        //
+        // TODO: Add constructor logic here
+        //
+    }
+
+    #region IHttpHandler Members
+
+    public bool IsReusable
+    {
+        get { return true; }
+    }
+
+    public void ProcessRequest(HttpContext context)
+    {
+        //Your target URL here
+        context.Response.Redirect("~/Home/Index");
+        context.Response.End();
+    }
+
+    #endregion
+}
